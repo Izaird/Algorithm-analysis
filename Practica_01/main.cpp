5fd4a9092438cc99577ce41ea203254ff4f77852
Izaird Mothelet <iza_alex3@hotmail.com>
2020-01-31 00:31:11 -0600

    debbuger file config

--- Practica_01/main.cpp
+++ Practica_01/main.cpp
@@ -1,6 +1,15 @@
 #include "ordering.h"
 
 int main(){
+    std::cout << "test";
+    std::cout << "test";
+    std::cout << "test";
+
     IntList doc_list("rand_1000");
+    std::cout << "test";
+    std::cout << "test";
+    std::cout << "test";
+    std::cout << "test";
+
     return 0;
 }
