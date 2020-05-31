ab09a41dcf4ae30fd59c10e3504b92ed0a3458f0
Izaird Mothelet <iza_alex3@hotmail.com>
2020-05-31 18:25:33 -0500

    README

---
+++ Practica_05/README.md
@@ -0,0 +1,5 @@
+Se necesitara tener instalados los siguientes modulos para poder utilizar la aplicacion 'numpy' y 'xlsxwriter'
+
+Una vez instalados estos modulos se procedera a correr el archivo main.py con un interprete de python 3
+La aplicacion pedira que se le proporcione el nombre del archivo el cual debera de estar guardado en la misma carpeta que main.py
+Una vez concluida la ejecucion la tabla creada para calcular lcs se guardara en el archivo 'Tabla.xlsx'
--- Practica_05/main.py
+++ Practica_05/main.py
@@ -1,5 +1,6 @@
 from lcs import *
-
+import time
+import tqdm
 def get_strings(file_name):
     while True:
         try:
@@ -7,15 +8,21 @@ def get_strings(file_name):
             strings = (file.read().split("\n\n"))
             return strings
         except FileNotFoundError:
-            filem_name = input("Archivo no existe intente de nuevo:")
+            file_name = input("Archivo no existe intente de nuevo:")
 
 if __name__ == "__main__":
 
     file_name = input("Nombre del archivo que desea analizar:")
+    start = time.time()
     strings = get_strings(file_name)
     table = Table(strings)
     table.set_cell_values()
-    # table.print_cells()
     lcs = table.lcs()
+    end = time.time()
     print("lcs = " + lcs)
     print(len(lcs))
+    print(end - start)
+    similarity_percentage  = (len(lcs)*100)/len(strings[0])
+    print("%.2f" % similarity_percentage + '%')
+    #I do not measure the time it takes to create the file that stores the table because it is not a necessary process to calculate lcs
+    table.print_cells()
